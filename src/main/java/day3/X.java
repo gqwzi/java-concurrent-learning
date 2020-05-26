03d3dd33f59255b54f8b415b04b98dc9690aff4c
gqwzi <15364917905@163.com>
2020-05-26 23:58:54 +0800

    解决原子性问题

---
+++ src/main/java/day3/X.java
@@ -0,0 +1,25 @@
+package day3;
+
+/**
+ * @Author gaoqiangwei
+ * @Date 2020/5/26 23:49
+ * @Description
+ */
+public class X {
+    synchronized void foo() {
+        //锁一个非静态方法
+    }
+
+    synchronized static void bar() {
+        //锁一个静态方法
+    }
+
+    Object obj = new Object();
+    void baz() {
+        //锁一段代码块
+        synchronized (obj) {
+            //临界区
+        }
+    }
+
+}
