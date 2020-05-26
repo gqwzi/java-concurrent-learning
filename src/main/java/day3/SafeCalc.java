def651135324c0603a8c71bfbc59eae427084aed
gqwzi <15364917905@163.com>
2020-05-27 00:17:58 +0800

    解决原子性

---
+++ src/main/java/day3/SafeCalc.java
@@ -0,0 +1,16 @@
+package day3;
+
+/**
+ * @Author gaoqiangwei
+ * @Date 2020/5/26 23:59
+ * @Description
+ */
+public class SafeCalc {
+    int value = 0;
+    int get() {
+        return value;
+    }
+    synchronized void addOne() {
+        value +=1;
+    }
+}
