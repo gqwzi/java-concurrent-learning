d80e88a2e2bc6dbeacdb2045db4f6c4523c6718b
gqwzi <15364917905@163.com>
2020-05-30 00:13:25 +0800

    等待通知机制

---
+++ src/main/java/day6/Allocator.java
@@ -0,0 +1,28 @@
+package day6;
+
+import java.util.List;
+
+/**
+ * @Author gaoqiangwei
+ * @Date 2020/5/30 00:00
+ * @Description
+ */
+public class Allocator {
+    private List<Object> als;
+    //一次性申请所有资源
+    synchronized boolean apply(Object from,Object to)throws Exception {
+        //经典写法
+        while (als.contains(from) || als.contains(to)) {
+            wait();
+        }
+        als.add(from);
+        als.add(to);
+        return true;
+    }
+    //归还资源
+    synchronized void free(Object from,Object to) {
+        als.remove(from);
+        als.remove(to);
+        notifyAll();
+    }
+}
